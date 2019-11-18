e258a1a804d0633e4f29a8c3bb96fe7f4fba126b
s00083446 <s00083446@mail.itsligo.ie>
2019-11-18 18:20:29 +0000

    initialised web project on Firestore and added config details to app-component, also registered a user, and routes working with authguard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component, Provider } from '@angular/core';
 import { FighterService } from './shared/fighter.service';
-// import * as firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 
 // // Initialize Firebase
-// const firebaseConfig = {
-//   apiKey: "***",
-//   authDomain: "***",
-//   databaseURL: "***,"
-// };
-// firebase.initializeApp(firebaseConfig);
+ const firebaseConfig = {
+  // tslint:disable-next-line: quotemark
+  apiKey: "AIzaSyC35IIq7yxHqiYM8c9k3Q8F6vKKx362GLo",
+  authDomain: 'fighterapp-ab993.firebaseapp.com',
+  databaseURL: 'https://fighterapp-ab993.firebaseio.com',
+  projectId: 'fighterapp-ab993',
+  storageBucket: 'fighterapp-ab993.appspot.com',
+  messagingSenderId: '346903218518',
+  appId: '1:346903218518:web:0ea551985a466970cc6ef7',
+  measurementId: 'G-GKZ47ZDV3N'
+ };
+ firebase.initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-root',
